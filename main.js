ad3d693842e58b391dcca985945dff5df0425216
dudapereirapmp <127161056+dudapereirapmp@users.noreply.github.com>
2024-03-06 19:47:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll("aba-conteudo");
 
-for(let i=0,i <botoes.length;i++){
+for(let i=0;i <botoes.length;i++){
     botoes[1].onclik=function(){
 
         for(let j=0;j<botoes.length;j++){
         botoes[j].classList.remove("ativo")
+        textos[j].classList.remove("ativo")
         }
-botoes[i].classList.add("ativo");
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo")
     }
     
 }
+const contadores = document.querySelectorAll("contador");
+
+contadores[0].textContent = "";
