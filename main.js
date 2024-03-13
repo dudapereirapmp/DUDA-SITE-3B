114a05bb676d34b581b479ce407fc72c749c2a5b
dudapereirapmp <127161056+dudapereirapmp@users.noreply.github.com>
2024-03-13 20:06:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,5 +15,42 @@ for(let i=0;i <botoes.length;i++){
     
 }
 const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date ("2024-10-05t00:00:oo");
+const tempoObjetivo2 = new Date ("2024-12-05t00:00:oo");
+const tempoObjetivo3 = new Date ("2024-12-30t00:00:oo");
+const tempoObjetivo4 = new Date ("2025-02-01t00:00:oo");
 
-contadores[0].textContent = "";
+const tempos = [ tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function calculatempo(tempoObjetivo){
+    let tempoAtual= new Date()
+    let tempoFinal= tempoObjetivo- tempoAtual;
+    let segundos =Math.floor (tempoFinal/1000);
+    let minutos = math.floor(segundos/60);
+    let horas= Math.floor(minutos/60);
+    let dias= Math.floor(horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+   if(tempoFinal > 0){
+    return dias + " dias " + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+   } else {
+    return "Prazofinalizado";
+   }
+
+}
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++ ){
+        contadores[i].textContent= calculatempo(tempo[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
