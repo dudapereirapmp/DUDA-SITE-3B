f38b8c475fc2c3ff083699689478c2ffa4ef5a26
dudapereirapmp <127161056+dudapereirapmp@users.noreply.github.com>
2024-04-04 21:14:47 -0300

    .

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const botoes = document.getElementsByClassName("botao");
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclik = function(){
 
         for(let j=0;j<botoes.length;j++){
-             botoes[j].classList.remove("ativo");
+             botoes[j].classList.add("ativo");
             textos[j].classList.remove("ativo");
         }
+        botoes[j].classList.add("ativo");
 
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
